93d97e7f5105aba08aa6aaa66173cd39bb855f63
1mbtw <nzxrx1060@gmail.com>
2025-11-16 20:56:45 +0100

    Added delete functionality for phone, email, and address; updated show_help commands

---
+++ src/handlers/delete_email.py
@@ -0,0 +1,31 @@
+from src.handlers.input_error import input_error
+from src.entities import AddressBook
+
+
+@input_error
+def delete_email(args: list[str], contacts: AddressBook) -> str:
+    """
+    Removes an email from the contact.
+    If email is not provided and only one exists, it will be removed.
+    """
+    if not args:
+        raise ValueError("No arguments provided.")
+
+    name, *rest = args
+    if name not in contacts:
+        return f"ERROR: contact '{name}' does not exist!"
+
+    record = contacts[name]
+    email_to_remove = rest[0] if rest else None
+
+    if email_to_remove is None:
+        if len(record.emails) == 1:
+            email_to_remove = str(record.emails[0])
+        else:
+            return "Please specify which email to delete."
+
+    if record.find_email(email_to_remove):
+        record.remove_email(email_to_remove)
+        return "Email deleted."
+
+    return "Nothing to delete."
---
+++ src/handlers/delete_phone.py
@@ -0,0 +1,31 @@
+from src.handlers.input_error import input_error
+from src.entities import AddressBook
+
+
+@input_error
+def delete_phone(args: list[str], contacts: AddressBook) -> str:
+    """
+    Removes a phone number from the contact.
+    If phone is not provided and only one exists, it will be removed.
+    """
+    if not args:
+        raise ValueError("No arguments provided.")
+
+    name, *rest = args
+    if name not in contacts:
+        return f"ERROR: contact '{name}' does not exist!"
+
+    record = contacts[name]
+    phone_to_remove = rest[0] if rest else None
+
+    if phone_to_remove is None:
+        if len(record.phones) == 1:
+            phone_to_remove = str(record.phones[0])
+        else:
+            return "Please specify which phone to delete."
+
+    if record.find_phone(phone_to_remove):
+        record.remove_phone(phone_to_remove)
+        return "Phone deleted."
+
+    return "Nothing to delete."
