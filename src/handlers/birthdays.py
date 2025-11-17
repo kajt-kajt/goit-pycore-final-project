f74cde3d7ac56b24e89ebed3157c50630ede9077
kajt <kajt.kajt.kajt.kajt.kajt@gmail.com>
2025-11-17 17:34:50 +0200

    fix birthday functionality

--- src/handlers/birthdays.py
+++ src/handlers/birthdays.py
@@ -2,17 +2,21 @@ from src.handlers.input_error import input_error
 from src.entities import AddressBook
 
 @input_error
-def birthdays(_: list[str], book: AddressBook, __):
+def birthdays(args: list[str], book: AddressBook, __):
     """
-    Display list of contacts for greeting during next week
+    Display list of contacts for greeting for the period of time specified.
+    By default period is 7 days, but may be provided as argument.
     """
-    upcoming_birthdays = book.get_upcoming_birthdays()
-    upcoming_birthdays.sort(key=lambda x: x["congratulation_date"])
-    result = "\n".join(
-        [
-            f"[{x['congratulation_date'].strftime('%d.%m.%Y')}] {x['record']}"
-            for x in upcoming_birthdays
-        ]
-    )
+    days = 7
+    try:
+        days = int(args[0])
+    except (ValueError, IndexError):
+        pass
+
+    users_with_birthdays = [record for record in book.values() if record.birthday]
+    upcoming_birthdays = [record for record in users_with_birthdays 
+                          if record.birthday.days_to_next_birthday() <= days]
+    upcoming_birthdays.sort(key = lambda x: x.birthday.days_to_next_birthday())
+    result = "\n".join([str(record) for record in upcoming_birthdays])
     return result
 
