5bfe902c3b58294995921ea2be1aea111d86822a
manul-Pa2 <mihatitov305@gmail.com>
2025-11-18 17:48:12 +0200

    New_add_id

--- src/handlers/add_note.py
+++ src/handlers/add_note.py
@@ -7,5 +7,6 @@ def add_note(args: list[str], _ , notes: NoteBook) -> str:
     Add note to NoteBook
     """
     note_text = args[0]
-    notes.add_note(Note(note_text))
-    return "Note added."
+    note = Note(note_text)
+    notes.add_note(note)
+    return f"Note added. ID: {note.id}"
