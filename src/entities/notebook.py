399063f513063a414516a81b733cd88c79b2de30
kajt <kajt.kajt.kajt.kajt.kajt@gmail.com>
2025-11-18 10:37:17 +0200

    Add text search by notes and for both books

--- src/entities/notebook.py
+++ src/entities/notebook.py
@@ -9,3 +9,10 @@ class NoteBook(UserDict):
     
     def add_note(self, note: Note) -> str:
         self[note.id] = note
+
+    def search_by_pattern(self, pattern: str) -> str:
+        """
+        Search all notes for a match of a pattern
+        """
+        results = [note.search_by_pattern(pattern) for note in self.values()]
+        return "\n\n".join([value for value in results if value])
